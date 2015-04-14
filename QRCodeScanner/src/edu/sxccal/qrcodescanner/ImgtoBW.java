4d733deaf2fd7659ec6171a3647ab53db9241333
Sayantan Majumdar <monti.majumdar@gmail.com>
2015-04-15 02:51:10 +0530

    Fixed indentations

--- QRCodeScanner/src/edu/sxccal/qrcodescanner/ImgtoBW.java
+++ QRCodeScanner/src/edu/sxccal/qrcodescanner/ImgtoBW.java
@@ -16,16 +16,16 @@ public class ImgtoBW
 	{
 		String bw=QRCode.filePath+"/BW.jpg";
 		ColorMatrix cm = new ColorMatrix();
-	    cm.setSaturation(0);
-	    ColorMatrixColorFilter cmf = new ColorMatrixColorFilter(cm);
+	    	cm.setSaturation(0);
+	    	ColorMatrixColorFilter cmf = new ColorMatrixColorFilter(cm);
 		Bitmap bmp=BitmapFactory.decodeFile(src);					
 		int sw=bmp.getWidth(),sh=(int)bmp.getHeight();
 		Bitmap bmp1=Bitmap.createScaledBitmap(bmp,(int)(sw*.7),(int)(sh*.7),true);
 		Bitmap bmp2=bmp1.copy(Bitmap.Config.ARGB_8888,true);			
 		Paint paint = new Paint();
-	    paint.setColorFilter(cmf);
-	    Canvas canvas = new Canvas(bmp2);
-	    canvas.drawBitmap(bmp2, 0, 0, paint);			
+	    	paint.setColorFilter(cmf);
+		Canvas canvas = new Canvas(bmp2);
+	    	canvas.drawBitmap(bmp2, 0, 0, paint);			
 		FileOutputStream fp=new FileOutputStream(bw);				
 		bmp2.compress(Bitmap.CompressFormat.JPEG,7,fp);
 		fp.close();
@@ -33,7 +33,7 @@ public class ImgtoBW
 	}
 }
 
-/*								***		LIBRARY OVERVIEW	***	                              						*/
+/*			***		LIBRARY OVERVIEW	***	               				*/
 
 /*ColorMatrix: 4x5 matrix for transforming the color and alpha components of a Bitmap
   Class Details: http://developer.android.com/reference/android/graphics/ColorMatrix.html
