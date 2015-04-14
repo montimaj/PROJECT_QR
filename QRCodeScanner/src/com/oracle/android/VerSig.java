e0fb834970e9340f750465e8fafcf37c4089e252
Sayantan Majumdar <monti.majumdar@gmail.com>
2015-04-15 02:46:45 +0530

    Fixed indentations

--- QRCodeScanner/src/com/oracle/android/VerSig.java
+++ QRCodeScanner/src/com/oracle/android/VerSig.java
@@ -13,33 +13,33 @@ import edu.sxccal.qrcodescanner.Verify;
 //Verifies input file
 public class VerSig 
 {
-	public static void verify(String args[]) throws Exception
+    public static void verify(String args[]) throws Exception
     {    	
     	FileInputStream keyfis = new FileInputStream(args[0]);
-	    byte[] encKey = new byte[keyfis.available()];  
-	    keyfis.read(encKey);
-	    keyfis.close();
-	    X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
-	    KeyFactory keyFactory = KeyFactory.getInstance("RSA", "AndroidOpenSSL");
-	    PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
-	    FileInputStream sigfis = new FileInputStream(args[1]);            
-	    byte[] sigToVerify = new byte[sigfis.available()]; 
-	    sigfis.read(sigToVerify);
-	    sigfis.close();
-	    Signature sig = Signature.getInstance("SHA1withRSA", "AndroidOpenSSL");
-	    sig.initVerify(pubKey);
-	    FileInputStream datafis = new FileInputStream(args[2]);            
-	    BufferedInputStream bufin = new BufferedInputStream(datafis);
-	    byte[] buffer = new byte[3072];
-	    int len;
-	    while (bufin.available() != 0)
-	    {
-	    	len = bufin.read(buffer);
+	byte[] encKey = new byte[keyfis.available()];  
+	keyfis.read(encKey);
+	keyfis.close();
+	X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
+	KeyFactory keyFactory = KeyFactory.getInstance("RSA", "AndroidOpenSSL");
+	PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
+	FileInputStream sigfis = new FileInputStream(args[1]);            
+	byte[] sigToVerify = new byte[sigfis.available()]; 
+	sigfis.read(sigToVerify);
+	sigfis.close();
+	Signature sig = Signature.getInstance("SHA1withRSA", "AndroidOpenSSL");
+	sig.initVerify(pubKey);
+	FileInputStream datafis = new FileInputStream(args[2]);            
+	BufferedInputStream bufin = new BufferedInputStream(datafis);
+	byte[] buffer = new byte[3072];
+	int len;
+	while (bufin.available() != 0)
+	{
+		len = bufin.read(buffer);
 	        sig.update(buffer, 0, len);
-	    }
-	    bufin.close();
-	    boolean verifies=sig.verify(sigToVerify);  	            
-	    Verify.tv.setText("Digital Signature verification result: "+verifies);		       		 
+	}
+	bufin.close();
+	boolean verifies=sig.verify(sigToVerify);  	            
+	Verify.tv.setText("Digital Signature verification result: "+verifies);		       		 
     }		
 }
 
