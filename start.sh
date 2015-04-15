486f5ebc014d13251c6ff98f0ff16a05629c2ba6
planckeinstein <mail2abhisek.maiti@gmail.com>
2015-04-15 17:57:51 +0530

    Updated GUI: Now Verfication also can be done using GUI NB: Still few bugs are present. May not work as exppected

--- start.sh
+++ start.sh
@@ -1,45 +1,111 @@
 #!/bin/bash
+
+
 export MAIN_DIALOG='
-<window title="GenQR">
-  <vbox>
-    
-    <frame Select Input File>
-      <hbox>
-	<entry>
-	  <variable>FILE</variable>
-	</entry>
-	<button>
-	  <input file stock="gtk-open"></input>
-	  <variable>FILE_BROWSE</variable>
-	  <action type="fileselect">FILE</action>
-	</button>
-      </hbox>
-    </frame>
-    
-    <frame Select Output Directory>
-      <hbox>
-	<entry accept="directory">
-	  <variable>FILE_DIRECTORY</variable>
-	</entry>
-	<button>
-	  <input file stock="gtk-open"></input>
-	  <variable>FILE_BROWSE_DIRECTORY</variable>
-	  <action type="fileselect">FILE_DIRECTORY</action>
-	</button>
-      </hbox>
-    </frame>
+
+<window title="Authentic QR">
+  
+  <notebook labels="Generate|Verify">
     
     <vbox>
-      <button>
-	<height>20</height>
-	<width>40</width>
-	<label>Generate</label>
-	<action signal="clicked">java GenQR $FILE $FILE_DIRECTORY</action>
-	<variable>"flag"</variable>
-      </button>
+    
+    <frame Select Input File>
+	  <hbox>
+	    <entry>
+	      <variable>FILE</variable>
+	    </entry>
+	    <button>
+	      <input file stock="gtk-open"></input>
+	      <variable>FILE_BROWSE</variable>
+	      <action type="fileselect">FILE</action>
+	    </button>
+	  </hbox>
+     </frame>
+    
+      <frame Select Output Directory>
+	<hbox>
+	  <entry accept="directory">
+	    <variable>FILE_DIRECTORY</variable>
+	  </entry>
+	  <button>
+	    <input file stock="gtk-open"></input>
+	    <variable>FILE_BROWSE_DIRECTORY</variable>
+	    <action type="fileselect">FILE_DIRECTORY</action>
+	  </button>
+	</hbox>
+      </frame>
+    
+      <vbox>
+	<button>
+	  <height>20</height>
+	  <width>40</width>
+	  <label>Generate</label>
+	  <action signal="clicked">
+		java GenQR $FILE $FILE_DIRECTORY
+	  </action>
+	  <variable>"flag"</variable>
+	</button>
+      </vbox>
+    
+    </vbox>
+    
+    <vbox>
+    
+      <frame Choose File to be Tested>
+	<hbox>
+	  <entry>
+	    <variable>XFILE</variable>
+	  </entry>
+	  <button>
+	    <input file stock="gtk-open"></input>
+	    <variable>xfile</variable>
+	    <action type="fileselect">XFILE</action>
+	  </button>
+	</hbox>
+      </frame>
+      
+      <frame Choose Corresponding Signature>
+	<hbox>
+	  <entry>
+	    <variable>XSIG</variable>
+	  </entry>
+	  <button>
+	    <input file stock="gtk-open"></input>
+	    <variable>pkey</variable>
+	    <action type="fileselect">XSIG</action>
+	  </button>
+	</hbox>
+      </frame>
+    
+      <frame Choose Appropriate Public Key>
+	<hbox>
+	  <entry>
+	    <variable>PKEY</variable>
+	  </entry>
+	  <button>
+	    <input file stock="gtk-open"></input>
+	    <variable>pkey</variable>
+	    <action type="fileselect">PKEY</action>
+	  </button>
+	</hbox>
+      </frame>
+    
+      <vbox>
+	<button>
+	  <height>20</height>
+	  <width>40</width>
+	  <label>Verify</label>
+	  <action signal="clicked">
+		java VerSig $XFILE $XSIG $PKEY
+	  </action>
+	</button>
+      </vbox>
+    
     </vbox>
    
-   </vbox>
+   </notebook>
+   
 </window>
+
 '
 gtkdialog --program=MAIN_DIALOG
